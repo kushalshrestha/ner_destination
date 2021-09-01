aebf994fefea743a68d7e34b1b3f67d33398f37b
Kushal Shrestha <kushalshr@gmail.com>
2021-09-01 11:45:49 +0545

    config file created for api

--- mercury_api.py
+++ mercury_api.py
@@ -3,48 +3,86 @@ import json
 import re
 import requests
 import spacy
+import configparser
+
  
 urls = (
-    '/(.*)', 'DestinationRequestHandler' #all maps to hello class
+    '/(.*)', 'DestinationRequestHandler' #all maps to destination request handler class
 )
 app = web.application(urls, globals())
  
  
 class DestinationRequestHandler:
-    
-    
-    def GET(self, name):
-        return name
-    
-    # def POST(self, collection):
-    #     data = web.input()
-    #     name, password = data.name, data.password
-    #     return name, password
-    #     return collection
-    #     data = web.data()
-    #     data_input = web.input()
-    #     return data
 
+    """list suggested places"""
     def POST(self, collection):
-        nlp = spacy.load(R".\training\model-best")
+        config = configparser.ConfigParser()
+        config.read('./configs/api_config.cfg')
+        model_folder = config.get('model_path','folder')
+        model =  config.get('model_path','model_to_execute')
+        
+        nlp = spacy.load(R''+model_folder+model)
+        
         itinerary_object = json.loads(web.data())
-        title_list = []
         suggestionsPlaces = []
         for itinerary_item in itinerary_object:
-            
             input_string = itinerary_item['title']
+            
             temp_string = input_string
             temp_string = re.sub(' - ', ' to ', temp_string)
-            temp_string = re.sub(r'[0-9]', '', temp_string)
+            temp_string = re.sub(' via ', ' through ', temp_string)
+            temp_string = re.sub(r"\([^()]*\)", "", temp_string)
+            temp_string = re.sub(r'[^a-zA-Z0-9 \n\.]', '', temp_string)
             temp_string = re.sub(r'/', '', temp_string)
-            title_list.append(temp_string)
+            
             doc = nlp(temp_string)
             doc.ents = set(doc.ents)
-            
+            place=[]
             for w in doc.ents:
-                suggestionsPlaces.append((input_string, w.text))
+                place.append(w.text)
+            item = {
+                    "product_id" : id,
+                    "day" : itinerary_item['day'],
+                    "input" : input_string, 
+                    "temp_string" : temp_string,
+                    "recommended_destination" : place
+                }
+            suggestionsPlaces.append(item)  
             
         return suggestionsPlaces
+
+    """only for test purpose"""
+    # def POST(self, collection):
+    #     suggestionsPlaces = []
+    #     nlp = spacy.load(R".\training\model-best")
+    #     for id in range(3000, 3300):
+    #         r = requests.get(url='https://www.bookmundi.com/ws/GetItineraryData?id=' + str(id))
+    #         itinerary_object = r.json()
+    #         if len(itinerary_object)>0:
+    #             for itinerary_item in itinerary_object:
+    #                 input_string = itinerary_item['title']
+    #                 temp_string = input_string
+    #                 temp_string = re.sub(' - ', ' to ', temp_string)
+    #                 temp_string = re.sub(' via ', ' through ', temp_string)
+    #                 temp_string = re.sub(r"\([^()]*\)", "", temp_string)
+    #                 temp_string = re.sub(r'[^a-zA-Z0-9 \n\.]', '', temp_string)
+    #                 temp_string = re.sub(r'/', '', temp_string)
+                    
+    #                 doc = nlp(temp_string)
+    #                 doc.ents = set(doc.ents)
+    #                 place=[]
+    #                 for w in doc.ents:
+    #                     place.append(w.text)
+    #                 item = {
+    #                         "product_id" : id,
+    #                         "day" : itinerary_item['day'],
+    #                         "input" : input_string, 
+    #                         "temp_string" : temp_string,
+    #                         "recommended_destination" : place
+    #                     }
+    #                 suggestionsPlaces.append(item)
+            
+    #     return suggestionsPlaces
  
  
 if __name__ == "__main__":
