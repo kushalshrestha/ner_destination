277409996bc6d0fbe7c4c4b501553173f1d23b06
Kushal Shrestha <kushalshr@gmail.com>
2021-09-01 13:17:01 +0545

    API response in JSON

--- mercury_api.py
+++ mercury_api.py
@@ -9,47 +9,54 @@ import configparser
 urls = (
     '/(.*)', 'DestinationRequestHandler' #all maps to destination request handler class
 )
+# Start web server
 app = web.application(urls, globals())
+web.header( 'Content-Type',
+            'application/json' )
  
  
 class DestinationRequestHandler:
-
+    
     """list suggested places"""
     def POST(self, collection):
-        config = configparser.ConfigParser()
-        config.read('./configs/api_config.cfg')
-        model_folder = config.get('model_path','folder')
-        model =  config.get('model_path','model_to_execute')
-        
-        nlp = spacy.load(R''+model_folder+model)
-        
+        model = self.__get_config()
+        nlp = spacy.load(model)
         itinerary_object = json.loads(web.data())
-        suggestionsPlaces = []
+        response_data = []
         for itinerary_item in itinerary_object:
-            input_string = itinerary_item['title']
-            
-            temp_string = input_string
-            temp_string = re.sub(' - ', ' to ', temp_string)
-            temp_string = re.sub(' via ', ' through ', temp_string)
-            temp_string = re.sub(r"\([^()]*\)", "", temp_string)
-            temp_string = re.sub(r'[^a-zA-Z0-9 \n\.]', '', temp_string)
-            temp_string = re.sub(r'/', '', temp_string)
-            
-            doc = nlp(temp_string)
+            input_string = self.__clean_text(itinerary_item['title'])
+            doc = nlp(input_string)
             doc.ents = set(doc.ents)
             place=[]
             for w in doc.ents:
                 place.append(w.text)
             item = {
-                    "product_id" : id,
+                    # "product_id" : id,
                     "day" : itinerary_item['day'],
-                    "input" : input_string, 
-                    "temp_string" : temp_string,
+                    "input" : itinerary_item['title'], 
+                    "temp_string" : input_string,
                     "recommended_destination" : place
                 }
-            suggestionsPlaces.append(item)  
-            
-        return suggestionsPlaces
+            response_data.append(item)  
+        return json.dumps(response_data)   
+        
+
+    def __get_config(self):
+        config = configparser.ConfigParser()
+        config.read('./configs/api_config.cfg')
+        model_folder = config.get('model_path','folder')
+        model = config.get('model_path','model_to_execute')
+        return model_folder + model
+
+    def __clean_text(self, input_string):
+        temp_string = input_string
+        temp_string = re.sub(' - ', ' to ', temp_string)
+        temp_string = re.sub(' via ', ' through ', temp_string)
+        temp_string = re.sub(r"\([^()]*\)", "", temp_string)
+        temp_string = re.sub(r'[^a-zA-Z0-9 \n\.]', '', temp_string)
+        temp_string = re.sub(r'/', '', temp_string)
+        return temp_string
+
 
     """only for test purpose"""
     # def POST(self, collection):
